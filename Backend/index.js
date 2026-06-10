82f900c2c1dba94a3276d450c94dbe31cb1cce14
Raijin-Cyber <ujjwalsharma2562005@gmail.com>
2026-06-10 22:10:14 +0530

    feat: Update CORS configuration to include dynamic frontend host

--- Backend/index.js
+++ Backend/index.js
@@ -37,7 +37,7 @@ export const io = new Server(httpServer, {
 
 configSocket(io); // configuing the web socket connection
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: ["http://localhost:5173", process.env.FRONTEND_HOST], credentials: true}));
 app.use(express.json()); // to parse all incoming request with valid JSON data type will be eventually converted to the JSON format.
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
